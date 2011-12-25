7d7cb2c67c62359cd045b4906fd947f3b36b9fff
pvanek <petr@yarpen.cz>
2011-12-25 22:01:05 +0000

    SVK translation finalized

--- translations/translate.sh
+++ translations/translate.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-PROJECT=librazorqt
-NO_OBSOLETE="-noobsolete"
+export PROJECT=librazorqt
+export OPTS="-noobsolete"
+export TARGET="-recursive .."
 
-if [ "$1" = "" ]; then
-    TS_FILE=${PROJECT}_`echo $LANG | awk -F"." '{print($1)}'`.ts
-else
-    TS_FILE=$1
-fi
-
-
-lupdate $NO_OBSOLETE -recursive ..  -ts $TS_FILE  && linguist $TS_FILE
-
+../../../scripts/translate-one.sh "$@"
