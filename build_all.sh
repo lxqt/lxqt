6baad7b4a5790f790323cfdbe00a1e796ca3398d
Kuzma Shapran <kuzma.shapran@gmail.com>
2013-10-26 13:16:08 +1300

    Ninja generator supported

--- build_all.sh
+++ build_all.sh
@@ -8,6 +8,8 @@
 #
 # example:
 # $ LIB_SUFFIX=64 ./build_all.sh
+# or
+# $ CMAKE_BUILD_TYPE=debug CMAKE_GENERATOR=Ninja CC=clang CXX=clang++ ./build_all.sh
 # etc.
 
 AUTOMAKE_REPOS=" \
@@ -66,6 +68,18 @@ else
 	CMAKE_INSTALL_PREFIX=""
 fi
 
+if env | grep -q ^CMAKE_GENERATOR= ; then
+	echo "x$CMAKE_GENERATOR"
+	if [ "x$CMAKE_GENERATOR" = "xNinja" ] ; then
+		CMAKE_MAKE_PROGRAM="ninja"
+		CMAKE_GENERATOR="-G $CMAKE_GENERATOR -DCMAKE_MAKE_PROGRAM=$CMAKE_MAKE_PROGRAM"
+	fi
+fi
+
+if [ "x$CMAKE_MAKE_PROGRAM" = "x" ] ; then
+	CMAKE_MAKE_PROGRAM="make"
+fi
+
 if env | grep -q ^LIB_SUFFIX= ; then
 	CMAKE_LIB_SUFFIX="-DLIB_SUFFIX=$LIB_SUFFIX"
 else
@@ -73,13 +87,13 @@ else
 fi
 
 
-ALL_CMAKE_FLAGS="$CMAKE_BUILD_TYPE $CMAKE_INSTALL_PREFIX $CMAKE_LIB_SUFFIX"
+ALL_CMAKE_FLAGS="$CMAKE_BUILD_TYPE $CMAKE_INSTALL_PREFIX $CMAKE_LIB_SUFFIX $CMAKE_GENERATOR"
 
 for d in $CMAKE_REPOS
 do
 	echo ""; echo ""; echo "building: $d using externally specified options: $ALL_CMAKE_FLAGS"; echo ""
 	mkdir -p $d/build
 	cd $d/build
-	cmake $ALL_CMAKE_FLAGS .. && make && sudo make install
+	cmake $ALL_CMAKE_FLAGS .. && $CMAKE_MAKE_PROGRAM && sudo $CMAKE_MAKE_PROGRAM install
 	cd ../..
 done
