c2c5f4bd94465a5fc7d933fe0bf00d7f3a37dc0a
Jerome Leclanche <jerome@leclan.ch>
2014-10-17 01:54:13 +0200

    Clean up CMakeLists.txt and drop Qt 4 support

--- cmake/FindInstallConfigPath.cmake
+++ cmake/FindInstallConfigPath.cmake
@@ -5,18 +5,16 @@
 #   qmake -query QT_INSTALL_CONFIGURATION
 #
 if(NOT DEFINED LXQT_ETC_XDG_DIR)
-    if (USE_QT5)
-        get_target_property(QT_QMAKE_EXECUTABLE ${Qt5Core_QMAKE_EXECUTABLE} IMPORTED_LOCATION)
-        message(STATUS "${QT_QMAKE_EXECUTABLE}")
-    endif()
+    get_target_property(QT_QMAKE_EXECUTABLE ${Qt5Core_QMAKE_EXECUTABLE} IMPORTED_LOCATION)
+    message(STATUS "${QT_QMAKE_EXECUTABLE}")
 
     if(NOT QT_QMAKE_EXECUTABLE)
-        message(FATAL_ERROR "LXQT_ETC_XDG_DIR: qmake not found or wrongly detected (inlude before qt configured?)")
+        message(FATAL_ERROR "LXQT_ETC_XDG_DIR: qmake executable not found (included before qt was configured?)")
     endif()
 
     execute_process(COMMAND ${QT_QMAKE_EXECUTABLE} -query QT_INSTALL_CONFIGURATION
-                        OUTPUT_VARIABLE LXQT_ETC_XDG_DIR
-                        OUTPUT_STRIP_TRAILING_WHITESPACE)
+                    OUTPUT_VARIABLE LXQT_ETC_XDG_DIR
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     message(STATUS "LXQT_ETC_XDG_DIR autodetected as '${LXQT_ETC_XDG_DIR}'")
     message(STATUS "You can set it manually with -DLXQT_ETC_XDG_DIR=<value>")
