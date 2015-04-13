af91c5bf2c50da8b9996a60f63fc57c1caf024a6
Luís Pereira <luis.artur.pereira@gmail.com>
2015-04-13 09:12:18 -0700

    Removes USE_QT5 documentation
    We don't use it anymore.

--- build_all.sh
+++ build_all.sh
@@ -5,8 +5,6 @@
 # LIB_SUFFIX can set the ${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}
 #     useful fro 64 bit distros
 # LXQT_PREFIX changes default /usr/local prefix
-# USE_QT5 environment variable chooses betweeen Qt4 and Qt5 build. An cmake
-#   script is used to read it. So it follows the cmake true/false rules for
 #
 # example:
 # $ LIB_SUFFIX=64 ./build_all.sh
