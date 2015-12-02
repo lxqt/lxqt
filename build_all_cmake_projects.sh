3a9059646d9295de1fcaedfbeb1238768fa67e54
Palo Kisa <palo.kisa@gmail.com>
2015-12-02 10:04:23 +0100

    build: Use more modern bash constructs

--- build_all_cmake_projects.sh
+++ build_all_cmake_projects.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 #
 # various options for cmake based builds:
 # CMAKE_BUILD_TYPE can specify a build (debug|release|...) build type
 # LIB_SUFFIX can set the ${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}
-#     useful fro 64 bit distros
+#     useful for 64 bit distros
 # LXQT_PREFIX changes default /usr/local prefix
 #
 # example:
@@ -42,31 +42,29 @@ OPTIONAL_CMAKE_REPOS=" \
 	compton-conf \
 	obconf-qt"
 
-if env | grep -q ^CMAKE_BUILD_TYPE= ; then
+if [[ -n "$CMAKE_BUILD_TYPE" ]]; then
 	CMAKE_BUILD_TYPE="-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"
 else
 	CMAKE_BUILD_TYPE="-DCMAKE_BUILD_TYPE=debug"
 fi
 
-if env | grep -q ^LXQT_PREFIX= ; then
+if [[ -n "$LXQT_PREFIX" ]]; then
 	CMAKE_INSTALL_PREFIX="-DCMAKE_INSTALL_PREFIX=$LXQT_PREFIX"
 else
 	CMAKE_INSTALL_PREFIX=""
 fi
 
-if env | grep -q ^CMAKE_GENERATOR= ; then
+if [[ -n  "$CMAKE_GENERATOR" ]]; then
 	#echo "x$CMAKE_GENERATOR"
-	if [ "x$CMAKE_GENERATOR" = "xNinja" ] ; then
+	if [[ "$CMAKE_GENERATOR" = "Ninja" ]]; then
 		CMAKE_MAKE_PROGRAM="ninja"
 		CMAKE_GENERATOR="-G $CMAKE_GENERATOR -DCMAKE_MAKE_PROGRAM=$CMAKE_MAKE_PROGRAM"
 	fi
 fi
 
-if [ "x$CMAKE_MAKE_PROGRAM" = "x" ] ; then
-	CMAKE_MAKE_PROGRAM="make"
-fi
+[[ -n "$CMAKE_MAKE_PROGRAM" ]] || CMAKE_MAKE_PROGRAM="make"
 
-if env | grep -q ^LIB_SUFFIX= ; then
+if [[ -n "$LIB_SUFFIX" ]]; then
 	CMAKE_LIB_SUFFIX="-DLIB_SUFFIX=$LIB_SUFFIX"
 else
 	CMAKE_LIB_SUFFIX=""
